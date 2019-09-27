40c5e835f7044eebdc34ed012c7e5e1cad7c023e
Ramchandra Joshi <rbjoshi1999@gmail.com>
2019-09-27 07:48:11 +0530

    Added Delete Account Functionality to the App

--- app/src/main/java/com/example/smartwallet/settings.java
+++ app/src/main/java/com/example/smartwallet/settings.java
@@ -1,21 +1,37 @@
 package com.example.smartwallet;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class settings extends AppCompatActivity {
 
     private Toolbar toolbar;
     private Button accountSettings, emailSettings, notificationSettings, passcode, deleteAccount;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
         toolbar = findViewById(R.id.app_toolbar);
         setSupportActionBar(toolbar);
@@ -59,7 +75,37 @@ public class settings extends AppCompatActivity {
         deleteAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(settings.this);
+                dialog.setTitle("Are You Sure?");
+                dialog.setMessage("Deleting your account will permenently delete your account from system.");
+                dialog.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        firebaseUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(settings.this, "Account Deleted Successfully", Toast.LENGTH_SHORT).show();
+                                    Intent loginActivity = new Intent(settings.this, LoginActivity.class);
+                                    startActivity(loginActivity);
+                                }
+                                else {
+                                    Toast.makeText(settings.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
 
+                dialog.setNegativeButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
             }
         });
     }
